075c681a5d8ee788f6807cbdf44608ee88304052
N Y E T <shawnmrzn29@gmail.com>
2026-05-30 21:31:17 +0800

    Update queuing.py

--- queuing.py
+++ queuing.py
@@ -1,3 +1,4 @@
+"""Used for storing, queue priority, treating pets, undoing registration, pet removal, and pet update"""
 class Queue:
     def __init__(self):
         self.items = []
@@ -5,20 +6,23 @@ class Queue:
     def enqueue(self, pets):
         index = 0
 
+        # Queue Priority comparison
         while index < len(self.items):
             if pets.severity < self.items[index].severity:
                 break
             index += 1
         self.items.insert(index, pets)
 
+    # Serves pet and removes from front of the queue
     def dequeue(self):
         if len(self.items) == 0:
             return None
 
         return self.items.pop(0)
 
+    # Used to check the current state of the queue
     def peek(self):
-        if len(self.items) == 0:
+        if len(self.items) == 0: # Avoid crashing
             print(f"\n{'=' * 60}")
             print("\nQueue is empty")
             print(f"\n{'=' * 60}")
@@ -33,12 +37,14 @@ class Queue:
                   f"Severity Level: {pets.severity} | ")
             print(f"\n{'=' * 100}")
 
+    # Used for updating, deleting, and undoing pet registration
     def remove(self, pet_id):
-        for i, pet in enumerate(self.items):
+        for i, pet in enumerate(self.items): # Checks the values of the index
             if pet.pet_id == pet_id:
                 del self.items[i]
                 return True
         return False
 
+    # Check queue size
     def size(self):
         return len(self.items)
