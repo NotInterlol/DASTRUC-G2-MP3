6b6e0fc863bf92c93f3fe271f844edf9c16aa13c
N Y E T <shawnmrzn29@gmail.com>
2026-05-30 19:40:49 +0800

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,19 +1,11 @@
 class Stack:
     def __init__(self):
-        self._items = []
+        self.items = []
 
-    def push(self, pet_id):
-        self._items.append(pet_id)
+    def push(self, pets):
+        self.items.append(pets)
 
     def pop(self):
-        if self.is_empty():
-            return None
-        return self._items.pop()
-
-    def peek(self):
-        if self.is_empty():
-            return None
-        return self._items[-1]
-
-    def is_empty(self):
-        return len(self._items) == 0
+        if self.items:
+            return self.items.pop()
+        return None
