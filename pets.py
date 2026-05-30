401ffdc488b2214df5be1a46bdeecd6019cbd416
N Y E T <shawnmrzn29@gmail.com>
2026-05-30 21:30:30 +0800

    Update pets.py

--- pets.py
+++ pets.py
@@ -1,12 +1,12 @@
-class Pets:
-    def __init__(self, pet_id, pet_name, breed, owner_name, severity):
-        self.pet_id = pet_id
-        self.pet_name = pet_name
-        self.breed = breed
-        self.owner_name = owner_name
-        self.severity = severity
+""" Used as a storage to register and undo registration"""
+class Stack:
+    def __init__(self):
+        self.items = []
 
-class Node:
-    def __init__(self, pets):
-        self.pets = pets
-        self.next = None
+    def push(self, pets):
+        self.items.append(pets)
+
+    def pop(self):
+        if self.items:
+            return self.items.pop()
+        return None
